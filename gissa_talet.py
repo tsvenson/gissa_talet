ad34ef416950e07d17c82007753eba0a077396e7
Thomas Svenson <thomas@tsvenson.com>
2017-09-03 21:09:24 +0200

    Lägger till en räknare för antalet gissningar.

--- gissa_talet.py
+++ gissa_talet.py
@@ -11,10 +11,12 @@ target = random.randint(1, 100)
 print('Välkommen till Gissa Talet spelet!')
 print('==================================')
 
+guesses = 0
 guess = 0
 
 while guess != target:
-    guess = int(input(f'Gissa vilket nummer jag tänker på: '))
+    guesses += 1
+    guess = int(input(f'Gissa vilket nummer jag tänker på [{guesses}]: '))
     if guess < target:
         print('För lågt gissat!')
     elif guess > target:
@@ -22,4 +24,4 @@ while guess != target:
 
 print('')
 print('Bra gjort!')
-print(f'Mitt nummer var {target} och du lyckades gissa rätt.')
+print(f'Mitt nummer var {target} och du lyckades gissa rätt på bara {guesses} försök.')
